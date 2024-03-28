b920635d26b96bd5a8d6eb7c1306a0d9fd6914c8
damiraskerbaev <askerbaev28@gmail.com>
2024-03-28 20:18:13 +0500

    urls

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,9 +25,3 @@ urlpatterns = [
     path('', include('apps.room.urls')),
     path('',include('apps.guest.urls'))
 ]
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
-
