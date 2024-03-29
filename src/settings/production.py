80090aefb96eb5c47d1ec274aeeabd19939f3ad0
damiraskerbaev <askerbaev28@gmail.com>
2024-03-30 01:46:45 +0500

    biraz_jeri_ozgerdi

--- src/settings/production.py
+++ src/settings/production.py
@@ -4,10 +4,7 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hotelexam$default',
-        'USER': 'hotelexam',
-        'PASSWORD': 'DaMiR2810',
-        'HOST': 'hotelexam.mysql.pythonanywhere-services.com'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'production.sqlite3',
     }
 }
